4acfd4656b5e7a666eb1cabe959ce39027a0096a
wpbindt <wesselbindt@gmail.com>
2020-12-26 14:45:52 +0100

    Add global mean value value model

---
+++ clv_model/clv_model/value_model/global_mean_value.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+import typing
+
+import pandas
+
+from value_model import ValueModel
+
+__all__ = ('GlobalMeanValue',)
+
+
+@dataclass
+class GlobalMeanValue(ValueModel):
+    def __post_init__(self):
+        self.global_mean: typing.Optional[float] = None
+
+    def fit(self, data: pandas.DataFrame) -> ValueModel:
+        total_transactions = data.frequency.sum()
+        self.global_mean = (
+            data
+            .assign(weighted_value=lambda df: df.value * df.frequency)
+            .weighted_value
+            .sum()
+            / total_transactions
+        )
+        return self
+
+    def _is_fitted(self) -> bool:
+        return self.global_mean is not None
+
+    def predict(self, data: pandas.DataFrame) -> pandas.DataFrame:
+        self._check_fit()
+        return (
+            data
+            .assign(value=self.global_mean)
+            [['customer_id', 'value']]
+        )
