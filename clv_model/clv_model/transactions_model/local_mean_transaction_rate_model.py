638db38614e8bbc902ad2d10591c11076950efe2
wpbindt <wesselbindt@gmail.com>
2020-12-26 15:59:15 +0100

    Add local mean transaction rate model

---
+++ clv_model/clv_model/transactions_model/local_mean_transaction_rate_model.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+from dataclasses import dataclass
+
+import pandas
+
+from clv_model.clv_model.transactions_model.transactions_model \
+    import TransactionsModel
+
+__all__ = ('LocalMeanTransactionRateModel',)
+
+
+@dataclass
+class LocalMeanTransactionRateModel(TransactionsModel):
+    def fit(self, data: pandas.DataFrame, **kwargs) -> TransactionsModel:
+        return self
+
+    def _is_fitted(self) -> bool:
+        return True
+
+    def predict(
+        self,
+        data: pandas.DataFrame,
+        periods: int
+    ) -> pandas.DataFrame:
+        return (
+            data
+            .assign(
+                transactions=lambda df: (df.frequency / df.T) * periods
+            )
+            [['id', 'transactions']]
+        )
