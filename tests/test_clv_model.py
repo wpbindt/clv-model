042daebd995861bf3d0549491f3e96df9bb6ce17
wpbindt <wesselbindt@gmail.com>
2021-01-13 23:40:16 +0100

    Refactor clv model tests

--- tests/test_clv_model.py
+++ tests/test_clv_model.py
@@ -10,16 +10,8 @@ from clv_model.value_model.global_mean_value import GlobalMeanValue
 
 
 class TestCLVModel(unittest.TestCase):
-    def test_predict(self) -> None:
-        value_model = GlobalMeanValue(global_mean=1)
-        transactions_model = GlobalTransactionRateModel(
-            mean_transaction_rate=1
-        )
-        model = CLVModel(
-            value_model=value_model,
-            transactions_model=transactions_model
-        )
-        data = pandas.DataFrame(
+    def _get_df(self) -> pandas.DataFrame:
+        return pandas.DataFrame(
             data={
                 'id': [0, 1, 2],
                 'recency': [1, 1, 1],
@@ -28,6 +20,20 @@ class TestCLVModel(unittest.TestCase):
                 'value': [1, 1, 2]
             }
         )
+
+    def _get_model(self) -> CLVModel:
+        value_model = GlobalMeanValue(global_mean=1)
+        transactions_model = GlobalTransactionRateModel(
+            mean_transaction_rate=1
+        )
+        return CLVModel(
+            value_model=value_model,
+            transactions_model=transactions_model
+        )
+
+    def test_predict(self) -> None:
+        data = self._get_df()
+        model = self._get_model()
         actual = model.predict(data=data, periods=1, discount_rate=0.15)
         expected = pandas.DataFrame(
             data={
@@ -41,6 +47,40 @@ class TestCLVModel(unittest.TestCase):
         ).assign(clv=lambda df: df.clv.round(2))
         assert_frame_equal(actual, expected)
 
+    def test_predict_no_discount(self) -> None:
+        data = self._get_df()
+        model = self._get_model()
+        actual = model.predict(data=data, periods=1, discount_rate=0)
+        expected = pandas.DataFrame(
+            data={
+                'id': [0, 1, 2],
+                'clv': [
+                    0.5 + 0.5 + 1,
+                    1 + 1 + 1,
+                    4 + 1
+                ]
+            }
+        ).assign(clv=lambda df: df.clv.round(2))
+        assert_frame_equal(actual, expected)
+
+    def test_predict_no_future(self) -> None:
+        data = self._get_df()
+        model = self._get_model()
+        actual = model.predict(data=data, periods=0, discount_rate=0.15)
+        expected = pandas.DataFrame(
+            data={
+                'id': [0, 1, 2],
+                'clv': [
+                    0.5 + 0.5/1.15,
+                    1 + 1/1.15,
+                    4,
+                ]
+            }
+        ).assign(clv=lambda df: df.clv.round(2))
+        assert_frame_equal(actual, expected)
+
+    def test_predict_empty(self) -> None:
+        model = self._get_model()
         actual = model.predict(data=pandas.DataFrame(
             columns={
                 'id',
